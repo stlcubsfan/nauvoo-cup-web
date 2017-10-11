22531c5f5b61b819da4607180bd7cbc546f03ece
Brian Coles <stlcubsfan@gmail.com>
2017-10-11 15:00:44 -0500

    Unit tests for list courses

--- src/app/create-course/create-course.component.spec.ts
+++ src/app/create-course/create-course.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule }   from '@angular/forms';
-
+import { RouterModule } from '@angular/router';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
 import { CreateCourseComponent } from './create-course.component';
 import { CourseService } from '../shared/course.service';
 
@@ -13,7 +15,7 @@ describe('CreateCourseComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ CreateCourseComponent ],
-      imports: [FormsModule],
+      imports: [FormsModule, RouterTestingModule],
       providers: [ {provide: CourseService, useValue: courseServiceStub}]
     })
     .compileComponents();
